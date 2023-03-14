139c7c8e890e6f75dd88667d64f6c15cef94323d
Spencer Christiansen <jc.spencer92@gmail.com>
2023-03-13 21:42:53 -0600

    Typecheck during dev

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  /**
+   * Custom Webpack Config
+   * https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+   */
+  webpack(config, options) {
+    const { dev, isServer } = options;
 
-module.exports = nextConfig
+    // Do not run type checking twice:
+    if (dev && isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
