d70da4c752045528d66d1915c918097a65895619
Spencer Christiansen <jc.spencer92@gmail.com>
2023-03-13 22:28:35 -0600

    Try building (it doesn't work)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Keep a Private & Shareable Journal</h1>
-    </main>
-  );
-}
+import * as React from 'react';
+
+const Home: React.FC<{}> = () => (
+  <main>
+    <h1>Keep a Private & Shareable Journal</h1>
+  </main>
+);
+
+export default Home;
