344f8cd963eee1181aad95072d7d15af252432ef
Spencer Christiansen <jc.spencer92@gmail.com>
2023-03-14 21:25:57 -0600

    Create some fluffy content & use tailwind typography
    The content needs a lot of work...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
 import * as React from 'react';
 
 const Home: React.FC<{}> = () => (
-  <main>
-    <h1>Keep a Private & Shareable Journal</h1>
+  <main className="prose prose-invert px-8 py-4">
+    <title>Nuvole</title>
+    <h1>Nuvole</h1>
+    <p>Keep a Private & Shareable Journal</p>
+    <p>
+      Nuvole is a free end-to-end encrypted journaling app with the ability to share specific
+      entries or whole journals with other users—without your journals ever being readable by the
+      Nuvole servers.
+    </p>
+    <p>
+      Your journal entries are yours. You can export your journals & entries in several convenient
+      formats, including plain text, markdown, and html.
+    </p>
+    <p>
+      Our client is completely open source, and we encourage others to implement their own clients.
+    </p>
   </main>
 );
 
